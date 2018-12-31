14cb0af6432f792f0b2b81171d89e756ebba8163
Ruhshan <ruhshan.abir@bkash.com>
2018-12-31 16:28:22 +0600

    updated settings

--- DialogHook/settings.py
+++ DialogHook/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
